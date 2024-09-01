540a2f37d25a93d2b7514398e703f18bfbbf3eca
rxs02230 <155025592+rxs02230@users.noreply.github.com>
2024-09-01 13:35:31 -0400

    init commit
    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      
+      <h1>Youtube clone App</h1>
+      <div>asdsadkljahskd</div>
+      <Button variant="contained" href="/">Hello world</Button>
+
+    </body>
+  
   );
 }
 
