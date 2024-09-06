6a609f9dd2414c079a082d11d0f984e55d3c745a
rxs02230 <155025592+rxs02230@users.noreply.github.com>
2024-09-05 20:14:51 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
+import { BrowserRouter, Routes, Route } from 'react-router-dom' ;
+import { Box } from '@mui/material' ;
 
 function App() {
   return (
     <body>
       
       <h1>Youtube clone App</h1>
-      <div>asdsadkljahskd</div>
+      <div>Clone</div>
       <Button variant="contained" href="/">Hello world</Button>
 
     </body>
